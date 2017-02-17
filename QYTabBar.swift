afc541dd3e3792e8dba1f61d013a97451844f7f3
weishouqiang <wsq724439564@126.com>
2017-02-17 18:04:11 +0800

    fix bug

--- QYTabBar.swift
+++ QYTabBar.swift
@@ -8,6 +8,9 @@
 
 import UIKit
 
+let topicColor = UIColor.init(colorLiteralRed: 255/255.0, green: 85/255.0, blue: 85/255.0, alpha: 1)
+let normalColor = UIColor.white
+
 class QYTabBarItem: UIButton {
     
     required public init?(coder aDecoder: NSCoder) {
@@ -16,41 +19,79 @@ class QYTabBarItem: UIButton {
     
     public init(frame: CGRect,title:String) {
         super.init(frame: frame)
-        let topicColor = UIColor.init(colorLiteralRed: 223, green: 89, blue: 88, alpha: 1)
+        
         self.layer.borderColor = topicColor.cgColor
         self.layer.borderWidth = 1.0
-        self.titleLabel?.textColor =  topicColor
+        self.titleLabel?.textColor = topicColor
         self.setTitle(title, for: UIControlState.normal)
+        self.setTitleColor(UIColor.white, for: UIControlState.selected)
+        self.setTitleColor(topicColor, for: UIControlState.normal)
+        self.backgroundColor = normalColor
     }
 }
 
 
 class QYTabBar: UIView {
+    var selectIndex:Int = 0;
+    var selectItem:UIButton?
+    var itemSpacing: CGFloat = 5
+    var verticalSpacing: CGFloat = 5
+    var itemCornerRedius: CGFloat = 0
     var delegate : QYTabBarDelegate?{
         didSet{
             layoutSubviews()
         }
     }
     override func layoutSubviews() {
+        
+        for sub in self.subviews{
+            sub.removeFromSuperview()
+        }
+        
         if (self.delegate != nil){
             let count = delegate?.tabBar(self, numberOfItems: 0)
-            let itemW = self.frame.size.width / CGFloat(count!)
-            let itemH = self.frame.size.height
+            let totalSpacing = itemSpacing * CGFloat(count!+1)
+            let itemW = (self.frame.size.width - totalSpacing) / CGFloat(count!)
+            let itemH = self.frame.size.height - verticalSpacing * 2
             for index in 0...2 {
                 let title = delegate?.tabBar(self, titleForItemAt: index)
                 let size = CGSize.init(width: itemW, height: itemH)
-                let item = QYTabBarItem.init(frame:CGRect.init(origin: CGPoint.init(x: CGFloat(CGFloat(index) * size.width), y: 0), size: size),title: title!)
+                let point = CGPoint.init(x: CGFloat(CGFloat(index) * (size.width + itemSpacing)) + itemSpacing, y: verticalSpacing)
+                let item = QYTabBarItem.init(frame:CGRect.init(origin: point, size: size),title: title!)
+                item.layer.cornerRadius = self.itemCornerRedius
                 item.addTarget(self, action: #selector(itemClicked), for: UIControlEvents.touchUpInside)
                 item.tag = index
                 self.addSubview(item)
+                if(index == selectIndex)
+                {
+                    updateSelect(item: item)
+                }
                 
             }
         }
     }
     
+    /// 更新选中的
+    ///
+    /// - parameter item:
+    func updateSelect(item:QYTabBarItem)  {
+        if selectItem != nil {
+            selectItem?.isSelected = false
+            selectItem?.backgroundColor = normalColor
+        }
+        
+        item.isSelected = true
+        selectItem = item;
+        selectIndex  = item.tag
+        item.backgroundColor = topicColor
+    }
     
+    /// 点击时间
+    ///
+    /// - parameter item:
     func itemClicked(item:QYTabBarItem)
     {
+        updateSelect(item: item)
         if self.delegate != nil{
             self.delegate?.tabBar(self, didSelectItemAt: item.tag)
         }
